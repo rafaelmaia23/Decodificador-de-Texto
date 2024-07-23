c0086d5f47f1af42f9228fd5b7a367f93238d8da
Rafael Maia <rafaelmfcontato@protonmail.com>
2024-07-23 18:41:21 -0300

    criadas funções de criptografia e descriptografia 

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -4,26 +4,56 @@ const encryptBtn = document.querySelector(".btn-criptografar");
 const decryptBtn = document.querySelector(".btn-descriptografar");
 const inputTextArea = document.querySelector(".input-text");
 const outputTextArea = document.querySelector(".output-text");
+const outputImg = document.querySelector(".output-area-img");
+const outputHeading = document.querySelector(".output-area-heading");
 
-function encrypt(input) {}
+function encrypt(input) {
+    const cryptKeys = getCryptKeys();
+    let encryptedInput = input;
+    for (const [key, value] of Object.entries(cryptKeys)) {
+        const regex = new RegExp(key, "g");
+        encryptedInput = encryptedInput.replace(regex, value);
+    }
+    return encryptedInput;
+}
 
-function decrypt(input) {}
+function decrypt(input) {
+    const cryptKeys = getCryptKeys();
+    let decryptedInput = input;
+    for (const [key, value] of Object.entries(cryptKeys)) {
+        const regex = new RegExp(value, "g");
+        decryptedInput = decryptedInput.replace(regex, key);
+    }
+    return decryptedInput;
+}
 
 function isInputValid(input) {
-    const regex = /^[a-z]+$/;
+    const regex = /^(?!\s*$)[a-z\s:,;!.?]+$/;
     return regex.test(input);
+}
+
+function getCryptKeys() {
+    return {
+        e: "enter",
+        i: "imes",
+        a: "ai",
+        o: "ober",
+        u: "ufat",
+    };
 }
 
 function writeOutput(output) {
     //TODO sumir com img e h2
+    outputImg.style.display = "none";
+    outputHeading.style.display = "none";
     outputTextArea.value = output;
 }
 
-encryptBtn.addEventListener(() => {
+encryptBtn.addEventListener("click", () => {
     const input = inputTextArea.value;
     if (!isInputValid(input)) {
         writeOutput(
-            "Input invalido! Testo deve ser apenas letras minusculas e não conter acentos."
+            "Input invalido! Texto não pode ser vazio, deve conter apenas letras minusculas e não ter acentos."
         );
         return;
     }
@@ -31,11 +61,11 @@ encryptBtn.addEventListener(() => {
     writeOutput(encryptedMsg);
 });
 
-decryptBtn.addEventListener(() => {
+decryptBtn.addEventListener("click", () => {
     const input = inputTextArea.value;
     if (!isInputValid(input)) {
         writeOutput(
-            "Input invalido! Testo deve ser apenas letras minusculas e não conter acentos."
+            "Input invalido! Texto não pode ser vazio, deve conter apenas letras minusculas e não ter acentos."
         );
         return;
     }
